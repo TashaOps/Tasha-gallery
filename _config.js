22b4d3d56f4742d4fd2193151991df3b872ce1f2
TashaOps <annastaciawangari18@gmail.com>
2023-09-18 16:58:42 +0300

    New MongoDB config

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://user-1:user1@cluster0.ck1dlgp.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://user-1:user1@cluster0.ck1dlgp.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://user-1:user1@cluster0.ck1dlgp.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
